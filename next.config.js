3a84b9a8be9601ddd66850877516c0d69bff7afb
12core1 <12533753+visnkmr@users.noreply.github.com>
2025-12-27 02:45:50 +0530

    convert to pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development'
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Enable static export
@@ -12,6 +17,6 @@ const nextConfig = {
   distDir: 'out'
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
 
 //updated dec25
